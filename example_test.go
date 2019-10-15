e49933acbdea4eaf2f8f13ac95622e77ecf1d8ad
YanJunhui <i@yanjunhui.com>
2019-10-15 17:01:48 +0800

    update

--- example_test.go
+++ example_test.go
@@ -19,7 +19,7 @@ import (
 	"io/ioutil"
 	"net/http"
 
-	"github.com/Unknwon/com"
+	"github.com/yanjunhui/com"
 )
 
 // ------------------------------
@@ -71,7 +71,7 @@ func ExampleGetGOPATHs() {
 }
 
 func ExampleGetSrcPath() {
-	srcPath, err := com.GetSrcPath("github.com/Unknwon/com")
+	srcPath, err := com.GetSrcPath("github.com/yanjunhui/com")
 	if err != nil {
 		fmt.Println(err)
 		return
